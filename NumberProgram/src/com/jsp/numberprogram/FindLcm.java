cfd4c55b5941c1785046050c548fbddec96a60a3
Rupesh Kumar <159168097+prajpatirupesh@users.noreply.github.com>
2025-03-10 10:18:54 +0530

    Solve lcm

--- NumberProgram/src/com/jsp/numberprogram/FindLcm.java
+++ NumberProgram/src/com/jsp/numberprogram/FindLcm.java
@@ -1,7 +1,18 @@
 package com.jsp.numberprogram;
 
 public class FindLcm {
+	public static void findLcm(int n1,int n2) {
+		int lcm=(n1<n2)? n1:n2;
+		while(true) {
+			if(lcm%n1==0 && lcm%n2==0) {
+				System.out.println(lcm);
+				break;
+			}
+			lcm++;
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("rUPESH KUMAR");
+		findLcm(12, 15);
 	}
 }
